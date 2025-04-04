07b06e166fae589725d43b77786c98becb4aec41
thomasraimondi <thomas.raimondi14@gmail.com>
2025-04-04 14:46:22 +0200

    Mail: add control on the user input

--- Mail/Js/main.js
+++ Mail/Js/main.js
@@ -1,15 +1,24 @@
 const invitedEmail = [
-  "mail1",
-  "mail2",
-  "mail3",
-  "mail4",
-  "mail5",
-  "mail6",
-  "mail7",
-  "mail8",
+  "pippo@pippo.it",
+  "pluto@pluto.com",
+  "me@gmail.com",
+  "lui@libero.it",
+  "asd@yahoo.it",
+  "qwe@outlook.com",
+  "zxc@gmail.com",
+  "cioa@icloud.com",
 ];
+let userEmail = "";
 
-const userEmail = prompt("Inserisci la tua mail:");
+do {
+  userEmail = prompt("Inserisci la tua mail:");
+  console.log(userEmail);
+} while (
+  !(
+    (userEmail.includes("@") && userEmail.endsWith(".it")) ||
+    userEmail.endsWith(".com")
+  )
+);
 console.log(`La mail dell'utente: ${userEmail}`);
 
 let isFindEmail = false;

0bb68b6c0cb0338991d4830e274bc2e957f250ee
thomasraimondi <thomas.raimondi14@gmail.com>
2025-04-04 14:20:31 +0200

    Mail: add condition to verify array and usermail

--- Mail/Js/main.js
+++ Mail/Js/main.js
@@ -10,9 +10,20 @@ const invitedEmail = [
 ];
 
 const userEmail = prompt("Inserisci la tua mail:");
-console.log(userEmail);
+console.log(`La mail dell'utente: ${userEmail}`);
 
-for (let i = 0; i < invitedEmail.length; i++) {
+let isFindEmail = false;
+let outputMessage = "";
+
+for (let i = 0; i < invitedEmail.length && isFindEmail === false; i++) {
   const currentValue = invitedEmail[i];
-  console.log(currentValue);
+  if (currentValue === userEmail) {
+    outputMessage = "Benvenuto alla festa ";
+    isFindEmail = true;
+    console.log(`Mail verificata: ${currentValue}`);
+  } else {
+    outputMessage = "La tua mail non è in lista";
+  }
 }
+
+alert(outputMessage);

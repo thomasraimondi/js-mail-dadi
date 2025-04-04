96ae61da5ee462941a3bf1a108643e97c4aaa82b
thomasraimondi <thomas.raimondi14@gmail.com>
2025-04-04 14:28:45 +0200

    Dadi: generate random numer for user and pc

---
+++ Gioco dei Dadi/Js/main.js
@@ -0,0 +1,13 @@
+const userNumber = Math.floor(Math.random(6) * 6) + 1;
+const pcNumber = Math.floor(Math.random(6) * 6) + 1;
+
+console.log(`Utente: ${userNumber}`);
+console.log(`PC: ${pcNumber}`);
+
+// if (userNumber > pcNumber) {
+//   console.log("L'utente ha vinto");
+// } else if (userNumber === pcNumber) {
+//   console.log("Pareggio");
+// } else {
+//   console.log("Il pc ha vinto");
+// }

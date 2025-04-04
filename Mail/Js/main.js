ed104484e7c425cb2f2a6550eabd21015de73ead
thomasraimondi <thomas.raimondi14@gmail.com>
2025-04-04 14:10:03 +0200

    first commit

---
+++ Mail/Js/main.js
@@ -0,0 +1,13 @@
+const invitedEmail = [
+  "mail1",
+  "mail2",
+  "mail3",
+  "mail4",
+  "mail5",
+  "mail6",
+  "mail7",
+  "mail8",
+];
+
+const userEmail = prompt("Inserisci la tua mail:");
+console.log(userEmail);

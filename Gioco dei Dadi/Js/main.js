566bfc69d8991af4fa4ef7c442bc8ce856a16392
thomasraimondi <thomas.raimondi14@gmail.com>
2025-04-04 14:29:24 +0200

    Dadi : add If to compare the number

--- Gioco dei Dadi/Js/main.js
+++ Gioco dei Dadi/Js/main.js
@@ -4,10 +4,10 @@ const pcNumber = Math.floor(Math.random(6) * 6) + 1;
 console.log(`Utente: ${userNumber}`);
 console.log(`PC: ${pcNumber}`);
 
-// if (userNumber > pcNumber) {
-//   console.log("L'utente ha vinto");
-// } else if (userNumber === pcNumber) {
-//   console.log("Pareggio");
-// } else {
-//   console.log("Il pc ha vinto");
-// }
+if (userNumber > pcNumber) {
+  console.log("L'utente ha vinto");
+} else if (userNumber === pcNumber) {
+  console.log("Pareggio");
+} else {
+  console.log("Il pc ha vinto");
+}
